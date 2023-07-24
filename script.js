63f3ad84c44e9c6c4eab1debbf08d8358051dd7a
Muhammed Yılmaz <yilmaz34muhammed@icloud.com>
2023-07-24 23:22:06 +0300

    message

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 const textareaEl = document.getElementById('textarea');
+
 const totalCounterEl = document.getElementById('total-counter');
+
 const remainingCounterEl = document.getElementById('remaining-counter');
+
 updateCounter();
 updateRemaining();
+
+//keyboard click to counter chnage
 textareaEl.addEventListener('keyup', () => {
+
     updateCounter();
     updateRemaining();
 });
+
+//total character counting
 function updateCounter() {
+
     totalCounterEl.innerText =  textareaEl.value.length;
 };
+
+//remainder character counting
 function updateRemaining () {
+
     remainingCounterEl.innerText = textareaEl.getAttribute('maxlength') 
      - textareaEl.value.length;
 };
